3702180aed8e3ba2fcf97e6fc5cc18e8503b793c
Jobin Biju <jobinbiju9090@gmail.com>
2021-09-29 12:49:23 +0530

    Parsed Json data & added to product card

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -4,22 +4,19 @@ import ProductCard from './ProductCard';
 import '../index.css';
 
 function ProductListing(props) {
-  return (
-    <div className="product-list">
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-    </div>
-  );
+  var jsonText =
+    '[{ "name":"Potato", "id":1, "price":30, "available":1, "vendor":"Himachal Pvt Ltd", "category":"Vegtables" }, { "name":"Banana", "id":2, "price":50, "available":1,"category": "Fruits","vendor": "Organic farms"}, { "name":"Drumsticks", "id":3, "price":20, "available":0, "category":"Vegetables", "vendor":"Mallikarjuna farms"}, { "name":"Orange", "id":4, "price":25, "available":1, "vendor":"Nagpur farms", "category":"Fruits"}]';
+  var products = JSON.parse(jsonText);
+  const item = products.map((product) => (
+    <ProductCard
+      title={product.name.toString()}
+      price={product.price}
+      availability={product.available === 1 ? true : false}
+      vendor={product.vendor}
+    />
+  ));
+
+  return <div className="product-list">{item}</div>;
 }
 
 ProductListing.propTypes = {};
